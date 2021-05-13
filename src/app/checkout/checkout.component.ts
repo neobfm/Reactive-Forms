e3bde89cfe940a3b2d1b4cdcb24667124d8fe820
neobfm <rkawa53@gmail.com>
2021-05-13 10:22:17 +0200

    commit-3

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
@@ -8,7 +8,10 @@ import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms'
 })
 export class CheckoutComponent implements OnInit {
 
-  checkoutform: FormGroup;
+   checkoutform: FormGroup;
+   itemArr = FormArray;
+
+   valueChangedTracked = '';
   constructor(private formBuilder: FormBuilder) {
     /*
     this.checkoutform = formBuilder.group({
@@ -19,7 +22,8 @@ export class CheckoutComponent implements OnInit {
       */
 
     this.checkoutform = formBuilder.group({
-      email: [ '', Validators.required, Validators.email],
+      // email: [ '', Validators.required, Validators.email],
+      email: [ '', Validators.required],
       quantity: [ '', Validators.required],
       terms: [ '', Validators.requiredTrue],
     });
@@ -27,10 +31,39 @@ export class CheckoutComponent implements OnInit {
   }
 
   ngOnInit(): void {
+
+    // track a single field only
+    this.checkoutform.get('email').valueChanges.subscribe(data => {
+     // console.log(data);
+     this.valueChangedTracked = data;
+    });
+     // track entire form
+    this.checkoutform.valueChanges.subscribe(data => {
+      console.log(data);
+    });
+
+    /*
+    this.checkoutform.setValue({
+      email: 'test@test.com', // setting values for single fields of the form for all fields
+      quantity: 5,
+      terms: true
+    });
+    */
+    this.checkoutform.patchValue({
+      email: 'test@test.com', // setting values for single fields of the form
+      quantity: 5,
+      // terms: true
+    });
   }
   // tslint:disable-next-line: typedef
   postData(){
-    console.log(this.checkoutform);
-    console.log(this.checkoutform.value);
+    // console.log(this.checkoutform);
+    console.log(this.checkoutform.value); // Gives all values of the entire form
+    console.log(this.checkoutform.value.email); // getting each value in single form
+    console.log(this.checkoutform.value.quantity);
+    console.log(this.checkoutform.value.terms);
+  }
+  resetForm(){
+    this.checkoutform.reset();
   }
 }
